936f1ff7bdd6e19ac74548bfe11d26dea08ceadc
Aditya Tiwari <91953193+Ady0313@users.noreply.github.com>
2023-02-15 10:00:07 +0530

    Create sorting.c

---
+++ sorting.c
@@ -0,0 +1,40 @@
+# bubble-sort
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+ //   printf("Hello world");
+
+int t;
+printf("Enter the Size of Array :");
+scanf("%d",&t);
+int a[t];
+printf("Enter the Values inside the array:\n");
+
+for(int i=0;i<t;i++){
+    printf("Value %d :",i+1);
+    scanf("%d",&a[i]);
+}
+int temp;
+
+for(int x=0;x<t;x++){
+    for(int j=x+1;j<t;j++){
+        
+        if(a[j]<a[x]){
+         temp =a[x];
+         a[x]=a[j];
+         a[j]= temp;
+        }
+    }
+}
+
+
+printf("Printing Sorted List:\n");
+for(int x=0;x<t;x++){
+    printf("%d\n",a[x]);
+}
+
+    return 0;
+}
